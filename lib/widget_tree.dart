53930777c4364e479e80263578b7b25be5330163
EhsanTgv <EhsanTgv@gmail.com>
2023-01-03 10:18:38 +0300

    Implement setup

---
+++ lib/widget_tree.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Widget Tree"),
+      ),
+    );
+  }
+}
