8136af6695c76ef04454c4982df63d3e24d60bc9
EhsanTgv <EhsanTgv@gmail.com>
2023-01-18 21:37:57 +0300

    Implement Responsive layout

--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_panel_dashboard_practice/responsive_layout.dart';
 
 class WidgetTree extends StatefulWidget {
   const WidgetTree({Key? key}) : super(key: key);
@@ -11,8 +12,19 @@ class _WidgetTreeState extends State<WidgetTree> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Widget Tree"),
+      appBar: PreferredSize(
+        preferredSize: const Size(double.infinity, 100),
+        child: (ResponsiveLayout.isTinyLimit(context) ||
+                ResponsiveLayout.isTinyHeightLimit(context)
+            ? Container()
+            : AppBar()),
+      ),
+      body: ResponsiveLayout(
+        tiny: Container(),
+        phone: Container(),
+        tablet: Container(),
+        largeTablet: Container(),
+        computer: Container(),
       ),
     );
   }
