e4488b9c2834083327a652de201526cd93cde7a6
EhsanTgv <EhsanTgv@gmail.com>
2023-02-24 11:47:26 +0300

    Implement Left panel

--- lib/panel_left/panel_left_page.dart
+++ lib/panel_left/panel_left_page.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:ui_panel_dashboard_practice/constants.dart';
+import 'package:ui_panel_dashboard_practice/panel_left/circle_graph.dart';
+import 'package:ui_panel_dashboard_practice/panel_left/curved_chart.dart';
+import 'package:ui_panel_dashboard_practice/responsive_layout.dart';
+
+class Todo {
+  String name;
+  bool enable;
+
+  Todo({this.enable = true, required this.name});
+}
 
 class PanelLeftPage extends StatefulWidget {
   const PanelLeftPage({Key? key}) : super(key: key);
@@ -8,11 +19,109 @@ class PanelLeftPage extends StatefulWidget {
 }
 
 class _PanelLeftPageState extends State<PanelLeftPage> {
+  final List<Todo> _todo = [
+    Todo(name: "Purchase Paper", enable: true),
+    Todo(name: "Refill the inventory of speakers", enable: true),
+    Todo(name: "Hire someone", enable: true),
+    Todo(name: "Marketing Strategy", enable: true),
+    Todo(name: "Selling furniture", enable: true),
+    Todo(name: "Finish the disclosure", enable: true),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
+    return Scaffold(
+      body: Stack(
+        children: [
+          if (ResponsiveLayout.isComputer(context))
+            Container(
+              color: Constants.purpleLight,
+              width: 50,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Constants.purpleDark,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: Constants.kPadding / 2,
+                    right: Constants.kPadding / 2,
+                    top: Constants.kPadding / 2,
+                  ),
+                  child: Card(
+                    color: Constants.purpleLight,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: const SizedBox(
+                      width: double.infinity,
+                      child: ListTile(
+                        title: Text(
+                          "Products Sold",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        subtitle: Text(
+                          "18% of Products Sold",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        trailing: Chip(
+                          label: Text(
+                            "4,500",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const LineChartSample2(),
+                const PieChartSample2(),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: Constants.kPadding / 2,
+                    right: Constants.kPadding / 2,
+                    top: Constants.kPadding / 2,
+                    bottom: Constants.kPadding,
+                  ),
+                  child: Card(
+                    color: Constants.purpleLight,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                      children: List.generate(
+                        _todo.length,
+                        (index) => CheckboxListTile(
+                          title: Text(
+                            _todo[index].name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          value: _todo[index].enable,
+                          onChanged: (newValue) {
+                            setState(() {
+                              _todo[index].enable = newValue ?? true;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
