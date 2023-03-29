1945d01cd8a60b3cc409030720b788ad782dc409
EhsanTgv <EhsanTgv@gmail.com>
2023-03-29 15:44:21 +0300

    Implement Right panel

--- lib/panel_right_page/panel_right_page.dart
+++ lib/panel_right_page/panel_right_page.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:ui_panel_dashboard_practice/constants.dart';
+import 'package:ui_panel_dashboard_practice/panel_right_page/line_chart_sample1.dart';
+
+class Product {
+  String name;
+  bool enable;
+
+  Product({this.enable = true, required this.name});
+}
 
 class PanelRightPage extends StatefulWidget {
   const PanelRightPage({Key? key}) : super(key: key);
@@ -8,11 +17,98 @@ class PanelRightPage extends StatefulWidget {
 }
 
 class _PanelRightPageState extends State<PanelRightPage> {
+  final List<Product> _products = [
+    Product(name: "LED Submersible Lights", enable: true),
+    Product(name: "Portable Projector", enable: true),
+    Product(name: "Bluetooth Speaker", enable: true),
+    Product(name: "Smart Watch", enable: true),
+    Product(name: "Temporary Tattoos", enable: true),
+    Product(name: "Bookends", enable: true),
+    Product(name: "Vegetable Chopper", enable: true),
+    Product(name: "Neck Massager", enable: true),
+    Product(name: "Facial Cleanser", enable: true),
+    Product(name: "Back Cushion", enable: true),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                right: Constants.kPadding / 2,
+                top: Constants.kPadding / 2,
+                left: Constants.kPadding / 2,
+              ),
+              child: Card(
+                color: Constants.purpleLight,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: const SizedBox(
+                  width: double.infinity,
+                  child: ListTile(
+                    title: Text(
+                      "Net Revenue",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "7% of sales Avg.",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    trailing: Chip(
+                      label: Text(
+                        r"$46,450",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const LineChartSample1(),
+            Padding(
+              padding: const EdgeInsets.only(
+                right: Constants.kPadding / 2,
+                bottom: Constants.kPadding,
+                top: Constants.kPadding,
+                left: Constants.kPadding / 2,
+              ),
+              child: Card(
+                color: Constants.purpleLight,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Column(
+                  children: List.generate(
+                    _products.length,
+                    (index) => SwitchListTile.adaptive(
+                      title: Text(
+                        _products[index].name,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      value: _products[index].enable,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _products[index].enable = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
