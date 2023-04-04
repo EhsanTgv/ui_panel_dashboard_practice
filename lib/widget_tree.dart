759c2d0eda605fe5eb438e228b091973bb309ea2
EhsanTgv <EhsanTgv@gmail.com>
2023-04-04 16:25:58 +0300

    Implement Curved Navigation bar

--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,5 +1,7 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_panel_dashboard_practice/app_bar/app_bar_widget.dart';
+import 'package:ui_panel_dashboard_practice/constants.dart';
 import 'package:ui_panel_dashboard_practice/drawer/drawer_page.dart';
 import 'package:ui_panel_dashboard_practice/panel_center/panel_center_page.dart';
 import 'package:ui_panel_dashboard_practice/panel_left/panel_left_page.dart';
@@ -14,6 +16,14 @@ class WidgetTree extends StatefulWidget {
 }
 
 class _WidgetTreeState extends State<WidgetTree> {
+  int currentIndex = 1;
+
+  final List<Widget> _icons = [
+    const Icon(Icons.add, size: 30),
+    const Icon(Icons.list, size: 30),
+    const Icon(Icons.compare_arrows, size: 30),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,11 +32,15 @@ class _WidgetTreeState extends State<WidgetTree> {
         child: (ResponsiveLayout.isTinyLimit(context) ||
                 ResponsiveLayout.isTinyHeightLimit(context)
             ? Container()
-            : AppBarWidget()),
+            : const AppBarWidget()),
       ),
       body: ResponsiveLayout(
         tiny: Container(),
-        phone: const PanelCenterPage(),
+        phone: currentIndex == 0
+            ? const PanelLeftPage()
+            : currentIndex == 1
+                ? const PanelCenterPage()
+                : const PanelRightPage(),
         tablet: Row(
           children: const [
             Expanded(child: PanelLeftPage()),
@@ -50,6 +64,18 @@ class _WidgetTreeState extends State<WidgetTree> {
         ),
       ),
       drawer: const DrawerPage(),
+      bottomNavigationBar: ResponsiveLayout.isPhone(context)
+          ? CurvedNavigationBar(
+              index: currentIndex,
+              backgroundColor: Constants.purpleDark,
+              items: _icons,
+              onTap: (index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+            )
+          : const SizedBox(),
     );
   }
 }
